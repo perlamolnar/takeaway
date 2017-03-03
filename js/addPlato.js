854fb04a5d953c1953d21774bfc90a8a40e4fcc1
perlamolnar <perla75molnar@gmail.com>
2017-03-03 19:16:56 +0100

    para añadir platos a las categorias

---
+++ js/addPlato.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	var debug=true;
+		$.ajax({
+			url:'../php/getSelectCat.php',
+			type: 'GET',
+			dataType: 'json',
+			success : function(result){
+				console.log (result);
+
+		        $.each(result.query, function() {  			//hace un bucle/loop que se repite para cada elemento de array
+		        	if (debug) console.log("Pintando");     //para controlar en consol
+		        	var options =" ";						//ofrecera todas las categoras (optiones)
+		        	$.each(this, function(campo, valor) {     		//(k: key/campo y v: value) pidemos que separe los campos de los valores de cada array
+	        			options += "<options>"+valor+"</options>";		//y ponga solo los valores sin campos en las cellas.
+	        		});	            	
+			    });
+		    	if (debug) console.log (options); 
+		    	//$("#categoria").html(options);  		// crear que aparezcan los elementos de categoria en el options en el html
+			},
+			error : function(result){
+				alert("errorrrrrrr!!!");
+			}
+		});
+
+});
