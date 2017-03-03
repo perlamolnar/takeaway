e9045c85837265a2c3a0dd7738b597be27a53b88
perlamolnar <perla75molnar@gmail.com>
2017-03-03 20:22:02 +0100

    añadido el select para los categorias

--- js/addPlato.js
+++ js/addPlato.js
@@ -6,16 +6,27 @@ $(document).ready(function() {
 			dataType: 'json',
 			success : function(result){
 				console.log (result);
-				var options =" ";	
-		        $.each(result.query, function() {  			//hace un bucle/loop que se repite para cada elemento de array
+				var etiquetas =" "; 		//etiquetas refieren al campo
+				var values =" ";			//refiere al values
+				var options =" ";			
+				
+		        $.each(result.query, function() {  			//hace un bucle/loop que se repite para cada elemento de array(recorre cada fila)
 		        	if (debug) console.log("Pintando");     //para controlar en consol
 		        						//ofrecera todas las categoras (optiones)
 		        	$.each(this, function(campo, valor) {     		//(k: key/campo y v: value) pidemos que separe los campos de los valores de cada array
-	        			options += "<options>"+valor+"</options>";		//y ponga solo los valores sin campos en las cellas.
-	        		});	            	
+	        			if (campo=="id") values=valor;
+	        			else etiquetas=valor;
+	            	});	 
+
+	            	options += "<option values='"
+	        						+values
+	        						+"'>"
+	        						+etiquetas
+	        						+"</option>"        	
 			    });
 		    	if (debug) console.log (options); 
-		    	//$("#categoria").html(options);  		// crear que aparezcan los elementos de categoria en el options en el html
+		    	$("#categoria").html(options);  		// crear que aparezcan los elementos de categoria en el options en el html
+				$('select').material_select();
 			},
 			error : function(result){
 				alert("errorrrrrrr!!!");
